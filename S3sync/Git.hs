02cb7bd27656795b43cee6b041d2850d478ccf03
Matthew Sorensen <scholl.ma@gmail.com>
2012-04-10 17:06:43 -0700

    Selects only the git changes we want: the working tree must match the index, not have any merge-conflicts, and must be tracked

--- S3sync/Git.hs
+++ S3sync/Git.hs
@@ -1,10 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
-module S3sync.Git (cdToToplevel, commit, clean, everything) where
+module S3sync.Git (cdToToplevel, commit, clean, everything, Change (..)) where
 
 import S3sync.Utils
 import Shelly hiding ((<$>))
 import Prelude hiding (init,lines,FilePath)
-import Data.Text.Lazy hiding (index)
+import Data.Text.Lazy hiding (index,all)
 import Control.Applicative
 import Filesystem.Path
 import Control.Monad
@@ -44,16 +44,6 @@ end = endOfLine >> endOfInput
 
 -- A parser for git-status' machine output format (-z), from http://schacon.github.com/git/git-status.html
 
-
-test = do
-  status <- git "status" ["-z"]
-  case parse (parseEntry `manyTill` end) status of
-    Done _ entries -> pure entries
-    Fail _ _ _ -> terror "Failed to parse git status output"
-
-
-
-
 data GitEntry = GitEntry {
       index::Action,
       work::Action,
@@ -81,8 +71,20 @@ action = choice $ mp <$> [(None,' '),(Modified, 'M'),
                           (Unmerged,'U'),(Ignore,'!'),(Ignore,'?')]
     where mp (act,c) = act <$ char c
 
+-- Only take the changes that we want - and rewrite all moves and copies in terms of add/delete.
 selectChange::GitEntry->[Change]
-selectChange = undefined
-
-
-
+selectChange e = if all ($ e) [tracked, unmodified, noConflicts] then rewrite e else []
+    where both f (GitEntry a b _ _) = f a && f b
+          tracked = both (/= Ignore)
+          -- The file in the working tree must match the index.
+          unmodified (GitEntry _ None _ _ ) = True
+          unmodified _ = False
+          -- This is picky, but any file that has merge conflicts shouldn't live.
+          noConflicts = both (/= Unmerged)
+          -- Rewrite the status entry in terms of actions we can take with s3:
+          rewrite (GitEntry Added _ f _) = [Write f]
+          rewrite (GitEntry Deleted _ f _) = [Delete f]
+          rewrite (GitEntry Modified _ f _) = [Write f]
+          rewrite (GitEntry Copied _ f _) = [Write f]
+          rewrite (GitEntry Renamed _ to (Just from)) = [Delete from, Write to]
+          rewrite _ = []
