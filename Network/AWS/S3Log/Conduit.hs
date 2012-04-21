a2c3687d07d20e3ac66d9ffc63182fbe1b6d4344
Matthew Sorensen <scholl.ma@gmail.com>
2012-04-21 12:00:15 -0700

    Conduit interface is done, pending figuring out if liftToEither is semantically valid w/re to finalizers.

--- Network/AWS/S3Log/Conduit.hs
+++ Network/AWS/S3Log/Conduit.hs
@@ -1,4 +1,4 @@
-module Network.AWS.S3Log.Conduit where
+module Network.AWS.S3Log.Conduit (streamLogEntries) where
 
 import Network.AWS.S3Log.Parser (safeLogParser)
 import Network.AWS.S3Log.Data hiding (key)
@@ -8,7 +8,7 @@ import Network.AWS.Monad
 import Network.AWS.AWSConnection (AWSConnection)
 import Network.AWS.S3Object (obj_data,S3Object)
 import Network.AWS.S3Bucket (ListRequest (..), key, listAllObjects)
-import Network.AWS.AWSResult (AWSResult,ReqError)
+import Network.AWS.AWSResult
 
 import Data.Conduit
 import Data.Conduit.Text
@@ -22,22 +22,20 @@ import Data.Text (Text)
 import qualified Data.Attoparsec.Text as A
 
 import Data.Conduit.Lazy
-import Data.Conduit.List (sourceList)
-
 
 data BucketState = Keys [String] | Error ReqError | Terminate
 type ResourceIO = ResourceT IO
 
-enumerateBucket::Bool->AWSConnection->String->Source ResourceIO (AWSResult S3Object)
-enumerateBucket del con bucket = sourceStateIO init none step
+streamBucket::Bool->AWSConnection->String->Source ResourceIO (AWSResult S3Object)
+streamBucket del con bucket = sourceStateIO init none step
     where none _ = return () -- We have no cleanup to perform
           -- Fetch a list of all the objects:
           init = fmap toBucketState $ listAllObjects con bucket $ ListRequest "" "" "" 1000
           toBucketState = either Error (Keys . map key)
           -- Fetch each object                                 
-          step Terminate = return $ StateClosed
+          step Terminate = return StateClosed
           step (Error e) = return $ StateOpen Terminate (Left e)
-          step (Keys []) = return $ StateClosed
+          step (Keys []) = return StateClosed
           step (Keys (x:rest)) = liftIO $ fmap (StateOpen $ Keys rest) $ runEitherT $ getMaybeDelete del con bucket x
 
 toText::(Monad m, MonadThrow m)=>Conduit ByteString m Text
@@ -51,11 +49,13 @@ type ParseResult a = Either String a
 -- Critically, the supplied parser must ALWAYS consume input upon failure - otherwise the conduit will return
 -- an infinite stream of parse errors. Ideally, it consumes all of the invalid input - although this is clearly
 -- a rather difficult task for any complex grammars.
+-- Note that this only parses from the Text values - the string is just a channel (... conduit! ...) for errors 
+-- and logging.
 
-attoparsec::(Monad m,MonadThrow m)=>A.Parser a->Conduit Text m (ParseResult a)
+attoparsec::(Monad m,MonadThrow m)=>A.Parser a->Conduit (Either String Text) m (Either String a)
 attoparsec parser = conduitState Finished push flush
     where consumeChunk acc st "" = (st, reverse acc)
-          consumeChunk acc st t  = case (getCont st) t of
+          consumeChunk acc st t  = case getCont st t of
                                      (A.Done rest new) -> consumeChunk (Right new : acc) Finished rest
                                      (A.Partial f)       -> (Parsing f, reverse acc)
                                      (A.Fail rest _ err) -> consumeChunk (Left  err : acc) Finished rest 
@@ -63,20 +63,18 @@ attoparsec parser = conduitState Finished push flush
           getCont Finished    = A.parse parser
           getCont (Parsing f) = f
 
-          push = ((return . uncurry StateProducing) .) . consumeChunk []
-
+          push st = return . either (StateProducing st . (:[]) . Left) (uncurry StateProducing . consumeChunk [] st)
+              
           flush Finished    = return []
           flush (Parsing f) = return $ case f "" of
-                                         (A.Done _ new) -> [Right new]  
+                                         (A.Done _ new)   -> [Right new]  
                                          (A.Partial _)    -> [Left "End of input"]
                                          (A.Fail _ _ err) -> [Left err] 
 
--- Now for the whole point of this file
-streamLogEntries::Bool->AWSConnection->String->Source ResourceIO (Either String S3Log)
-streamLogEntries = undefined
-
-
--- I really really don't think I'm supposed to be able to do this. But I can. 
+-- I really really don't think I'm supposed to be able to do this. But I can.
+-- Note that this totally messes up ordering of inputs - Left values pass directly 
+-- through, while the inner conduit gets to interact with Right values.
+ 
 liftToEither::Functor m=>Conduit i m o->Conduit (Either a i) m (Either a o)
 liftToEither (HaveOutput new final out) = HaveOutput (liftToEither new) final $ Right out
 liftToEither (NeedInput cont early) = let lifted = NeedInput newCont $ liftToEither early
@@ -85,3 +83,10 @@ liftToEither (NeedInput cont early) = let lifted = NeedInput newCont $ liftToEit
                                       in lifted
 liftToEither (Done m r) = Done (fmap Right m) r
 liftToEither (PipeM act f) = PipeM (fmap liftToEither act) f
+
+-- Now for the whole point of this file:
+streamLogEntries::Bool->AWSConnection->String->Source ResourceIO (Either String S3Log)
+streamLogEntries del con bucket = mapOutput getBody (streamBucket del con bucket) 
+                                  $= liftToEither toText 
+                                  $= attoparsec safeLogParser
+    where getBody = either (Left . prettyReqError) (Right . obj_data)
